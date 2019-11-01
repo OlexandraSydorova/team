fb52fafcb1a166a114af83696f68c63a281a0f8b
Your Name <you@example.com>
2019-11-01 11:55:22 +0200

    Add file

---
+++ ReturnKursNumber/ReturnKursNumber.py
@@ -0,0 +1,20 @@
+import re
+def define_kurs(numb_group):
+
+    """
+
+    :param numb_group: отримати назву групи, по ній визначити курс
+    :return:
+    """
+
+
+    # if bool(re.match(r"^[А-Я]{2}-9",numb_group)):
+    #     print("Перщий курс")
+    # if bool(re.match(r"^8",numb_group)):
+    #     print("Другий курс")
+    # if bool(re.match(r"^7",numb_group)):
+    #     print("Третій курс")
+    # if bool(re.match(r"^6",numb_group)):
+    #     print("Четвертий курс")
+
+    return None
